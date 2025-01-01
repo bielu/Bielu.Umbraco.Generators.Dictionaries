d1136766ac2631393c0cd1a54ff9598eb2e53ca5
Arkadiusz Biel - bielu <bielu@bielu.pl>
2025-01-01 11:51:47 +0000

    introduced configuration schema

--- src/Bielu.Umbraco.Generators.Dictionaries/Services/IDictionaryConstantModelBuilder.cs
+++ src/Bielu.Umbraco.Generators.Dictionaries/Services/IDictionaryConstantModelBuilder.cs
@@ -1,5 +1,6 @@
 ﻿using Umbraco.Cms.Core.Models;
-
+//todo: adjust namespace in v2
+// ReSharper disable once CheckNamespace
 namespace Bielu.Umbraco.Generators.Dictionaries;
 
 public interface IDictionaryConstantModelBuilder
---
+++ src/SchemaGenerator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using bielu.SchemaGenerator.Build.Configuration;
+using bielu.SchemaGenerator.Build.Services;
+using Bielu.Umbraco.Generators.Dictionaries.Configuration;
+using CommandLine;
+
+namespace SchemageGerator;
+
+internal static class Program
+{
+    private static readonly IList<Assembly> _assemblies = new List<Assembly>()
+    {
+        typeof(BieluDictionariesUmbracoSchemaDefinition).Assembly
+    };
+
+    public static async Task Main(string[] args)
+    {
+        try
+        {
+            await Parser.Default.ParseArguments<Options>(args)
+                .WithParsedAsync(Execute);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+
+    private static Task Execute(Options options)
+    {
+        Console.WriteLine("Schema generator v {0}", typeof(SchemaGeneratorService).Assembly.GetName().Version?.ToString());
+
+
+        var schemaGenerator = new SchemaGeneratorService(new SchemaGenerator(), options);
+        schemaGenerator.GenerateSchema(_assemblies);
+
+        Console.WriteLine("Schema generator v {0}", typeof(SchemaGeneratorService).Assembly.GetName().Version?.ToString());
+        return Task.CompletedTask;
+    }
+}
